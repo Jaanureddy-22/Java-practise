8aaa193497e0f175165d56c56b8fe6a10caed616
jaahnavi <jahnavireddybodepati@gmail.com>
2025-10-12 16:39:59 +0530

    gambling game

---
+++ gambling.java
@@ -0,0 +1,16 @@
+import java.util.*;
+//import java.lang.*;
+//import java.io.*;
+
+class gambling
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(7-a);
+		sc.close();
+
+	}
+}
