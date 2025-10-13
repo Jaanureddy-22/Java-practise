fc93e324344e3d774d068250b64b766d258b0f6d
jaahnavi <jahnavireddybodepati@gmail.com>
2025-10-13 19:42:24 +0530

    count of playlist

---
+++ playlist.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+class playlist
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-- > 0)
+		{
+		    int n=sc.nextInt();
+		    int x=sc.nextInt();
+		    System.out.println(n/(3*x));
+		}
+        sc.close();
+
+	}
+}
