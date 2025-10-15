0459382e048d4cd003d06b72bd6dfa1376cdbed4
jaahnavi <jahnavireddybodepati@gmail.com>
2025-10-15 10:06:58 +0530

    greater average

---
+++ Greater_avg.java
@@ -0,0 +1,28 @@
+import java.util.*;
+//import java.lang.*;
+//import java.io.*;
+
+class Greater_avg
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-- > 0)
+		{
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    if((a+b)/2.0 > c)
+		    {
+		        System.out.println("YES");
+		    }
+		    else{
+		         System.out.println("NO");
+		    }
+		}
+        sc.close();
+	}
+}
+		    
